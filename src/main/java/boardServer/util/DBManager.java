c6b349e6f6dceade25e6ff567e0397f48f14d3e3
5windy <5windy@naver.com>
2024-05-01 09:49:24 +0900

    Create Util DBManager

---
+++ src/main/java/boardServer/util/DBManager.java
@@ -0,0 +1,26 @@
+package boardServer.util;
+
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class DBManager {
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Context init = new InitialContext();
+			DataSource source = (DataSource) init.lookup("java:comp/env/jdbc/BoardServerDB");
+			conn = source.getConnection();
+			
+			System.out.println("[DB 연동 성공]");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("[DB 연동 실패]");
+		}
+		return conn;
+	}
+
+}
